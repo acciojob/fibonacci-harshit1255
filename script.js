680d58b26aaf6a6b0fded1521d8f23f1b257a12f
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-11-27 04:13:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function fibonacci(num) {
 // your code here
 	let fib = [0,1];
+	if(num ==1)
+		return 0;
+	else if (n==2)
+		return 1;
 	for(let i=0;i<num-2;i++)
 		{
 			fib.push(fib[fib.length-1]+fib[fib.length-2]);
